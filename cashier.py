0bc2209cd37ff169ae93f624fbce594410c62a9a
Troy <tevans55@charlotte.edu>
2024-10-01 04:00:37 -0400

    feat: finalized machine

--- cashier.py
+++ cashier.py
@@ -5,9 +5,21 @@ class Cashier:
     def process_coins(self):
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        print("Please insert your coins.")
+        total = int(input("How many quarters?")) * 0.25
+        total += int(input("How many dimes?")) * 0.1
+        total += int(input("How many nickels?")) * 0.05
+        total += int(input("How many pennies?")) * 0.01
+        return total
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
-        ##
+        if coins >= cost:
+            change = round(coins - cost, 2)
+            print(f"Here is ${change} in change.")
+            return True
+        else:
+            print("Sorry, you have insufficient funds. Your money has been refunded.")
+            return False
+
--- main.py
+++ main.py
@@ -13,7 +13,24 @@ cashier_instance = ######
 
 
 def main():
-    ###  write the rest of the codes ###
+    is_on = True
+
+    while is_on:
+        choice = input("What size sandwich do you want? (small/medium/large): ").lower()
+
+        if choice in recipes:
+            sandwich = recipes[choice]
+            if sandwich_maker_instance.check_resources(sandwich['ingredients']):
+                payment = cashier_instance.process_coins()
+                if cashier_instance.transaction_result(payment, sandwich['cost']):
+                    sandwich_maker_instance.make_sandwich(choice, sandwich['ingredients'])
+        elif choice == 'off':
+            is_on = False
+            print("Powering off machine")
+        elif choice == 'report':
+            print(resources)
+        else:
+            print("Invalid option. Choose again")
 
 if __name__=="__main__":
     main()
